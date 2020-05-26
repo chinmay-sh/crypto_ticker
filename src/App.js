c23365b8502a8bf0b432b09c10d73c531fedc363
Chinmay <chinmaysharma45@gmail.com>
2020-05-26 16:54:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [btcprice,setBtcPrice] = useState(0);
+  const [status,setStatus] = useState('NoGo')
+
+  useEffect(()=>{
+    setInterval(async ()=>{
+        const response = await axios.get('https://api.coinlore.net/api/ticker/?id=90');
+        //const response = await axios.get('https://api.exchangeratesapi.io/latest?symbols=INR&base=USD');
+        console.log('>>>>>>>>>>>>>')
+        console.log(response.data[0].price_usd)
+        if(response.data[0].price_usd !== btcprice){
+          setBtcPrice(response.data[0].price_usd);
+          setStatus('Go');
+        }else{
+          setStatus('NoGo');
+        }
+    },5000)
+        
+  },[btcprice]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      BTC to USD:
+      <br/>
+      Price: {' '}
+        <p>{btcprice}</p>
+
+      <div>
+        Status: {' '} <p>{status}</p>
+      </div>
     </div>
   );
 }
