88882821394aa49bb0f95bae564dd70b5e2181c9
Chinmay <chinmaysharma45@gmail.com>
2020-05-28 13:53:37 +0530

    add logo and fallback cryptocompare api

--- src/components/App.js
+++ src/components/App.js
@@ -6,39 +6,60 @@ import redTriangle from './assets/redT.png';
 function App() {
   const [btcprice,setBtcPrice] = useState(0);
   const [status,setStatus] = useState('red');
+  const [system,setSystem] = useState('cryptocompare');
   
   var previousPrice = 0;
 
   async function apiCall(){
-    previousPrice = btcprice;
-    const response = await axios.get('https://api.coinlore.net/api/ticker/?id=90');
-    //const response = await axios.get('https://api.exchangeratesapi.io/latest?symbols=INR&base=USD');
-    console.log('>>>>>>>>>>>>>')
-    console.log(response.data[0].price_usd)
-    if(response.data[0].price_usd !== btcprice){
-      setBtcPrice(response.data[0].price_usd);
-    }
-  }
+    if (system === 'coinlore'){
+      previousPrice = btcprice;
+      console.log(`Previous -> ${previousPrice}`);
+      const response = await axios.get('https://api.coinlore.net/api/ticker/?id=90');
+      const returnedPrice = response.data[0].price_usd;
+      console.log(`New Response -> ${returnedPrice}`)
+      if(returnedPrice !== btcprice){
+        setBtcPrice(returnedPrice);
 
-  function setImage(){
-    if(btcprice > previousPrice){
-    setStatus('green');
-    }else if(btcprice < previousPrice) {
-      setStatus('red');
+        if(returnedPrice > previousPrice){
+          console.log('Set Green')
+          setStatus('green');
+        } else if (returnedPrice < previousPrice){
+          console.log('Set Red')
+          setStatus('red');
+        }
+      }
+    }
+
+    else if(system === 'cryptocompare'){
+      previousPrice = btcprice;
+      console.log(`Previous -> ${previousPrice}`);
+      const response = await axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD');
+      const returnedPrice = response.data.USD;
+      console.log(`New Response -> ${returnedPrice}`)
+      if(returnedPrice !== btcprice){
+        setBtcPrice(returnedPrice);
+
+        if(returnedPrice > previousPrice){
+          console.log('Set Green')
+          setStatus('green');
+        } else if (returnedPrice < previousPrice){
+          console.log('Set Red')
+          setStatus('red');
+        }
+      }
     }
   }
 
   useEffect(()=>{
+    console.log('call 1');
     apiCall();
-    setImage()
     const timer = setInterval(()=>{
-      console.log('call 2')
-        apiCall()
-        setImage()
-    },5000)
+      console.log('call 2');
+        apiCall();
+    },8000)
     
     return () => clearInterval(timer);
-  },[btcprice,status]);
+  },[btcprice,system]);
 
   return (
     <div>
@@ -46,6 +67,7 @@ function App() {
       <br/>
       Price: {' '}
         <p>{btcprice}</p>
+        
 
       <div>
        Status: {' '} <img src={status === 'green' ? greenTriangle:redTriangle} />
